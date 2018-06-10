46308012b3c6f0503d844c3be0b60ec0280d2c58
oRlex <gumersinda88@gmail.com>
2018-06-10 14:36:51 +0300

    done

---
+++ js/build.js
@@ -0,0 +1,7 @@
+$(function () {
+    console.log('ham');
+    $('._hamburger').click(function () {
+        $('._nav').slideToggle();
+    });
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInRlc3QuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImJ1aWxkLmpzIiwic291cmNlc0NvbnRlbnQiOlsiJChmdW5jdGlvbiAoKSB7XG4gICAgY29uc29sZS5sb2coJ2hhbScpO1xuICAgICQoJy5faGFtYnVyZ2VyJykuY2xpY2soZnVuY3Rpb24gKCkge1xuICAgICAgICAkKCcuX25hdicpLnNsaWRlVG9nZ2xlKCk7XG4gICAgfSk7XG59KTsiXX0=
